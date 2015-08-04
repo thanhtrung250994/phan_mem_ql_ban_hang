20fd62487b60564cfae8cac522242b58d973ab0b
trungtt <thanhtrung250994@gmail.com>
2015-08-04 10:46:18 +0700

    Tạo class quản lí các thiết bị ngoại vi
    Tạo class quản lí các thiết bị ngoại vi

---
+++ thiet_bi_ngoai_vi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quanlicuahanggiaydep
+{
+    /// <summary>
+    /// class này sẽ đảm nhận trách nhiệm tao và kết nối với một số thiết bị ngoại vi(vd máy quét mã vạch)
+    /// </summary>
+    class thiet_bi_ngoai_vi
+    {
+        
+    }
+}
